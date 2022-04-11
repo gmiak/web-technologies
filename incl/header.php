c35cd8a54351214bb351de85da805cbe6d654200
gmiak <kayss.g@gmail.com>
2022-04-11 14:04:46 +0200

    About do some refactoring

--- incl/header.php
+++ incl/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=2.0;">
     <title> <?=$title ?> </title>
-    <link href="<?=$stylesheet?>" rel="stylesheet">
+    <link href="css/third.css" rel="stylesheet">  <!-- <link href="<?=$stylesheet?>" rel="stylesheet"> -->
     <link rel='shortcut icon' href='img/favicon.ico'/>
 </head>
 
