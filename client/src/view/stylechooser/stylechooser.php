78df28de112e8f377010a178fed1d8ae5069c5bc
gmiak <kayss.g@gmail.com>
2022-04-13 16:49:55 +0200

    Fixed stylechooser

--- client/src/view/stylechooser/stylechooser.php
+++ client/src/view/stylechooser/stylechooser.php
@@ -1,15 +1,16 @@
 
-<?
+<?php
 
 
 // Get the selected page, or the default one
-$pagestyle = (isset($_GET['stylepage'])) ? $_GET['stylepage'] : "intro";
+$pagestyle = isset($_GET['stylepage']) ? $_GET['stylepage'] : 'intro';
 
-
+is_string($pagestyle) or is_null($pagestyle)
+  or die("Incoming value for page must be a string.");
 
 // Where are the content-files
 $dir  = __DIR__ . "/content_stylechooser";
-
+$file = null;
 
 
 // Array with all valid pages
@@ -32,7 +33,6 @@ if (isset($multipagestyle[$pagestyle])) {
 } else {
     die("The value of ?page=" . htmlentities($pagestyle) . " is not recognized as a valid page.");
 }
-
 
 
 ?>
